5132cb0e1c50cf2ad89a0a3014f2bfa179c03464
Amol Chavan <amol@satvix.com>
2019-09-04 14:59:10 +0530

    Amol | Updated sql for latest Treatement line

--- openmrs/apps/reports/sql/List_of_Patients_not_suppressed.sql
+++ openmrs/apps/reports/sql/List_of_Patients_not_suppressed.sql
@@ -26,7 +26,7 @@ select distinct
    paddress.postal_code AS "Perto De",
    treatment_line.concept_full_name as "Última Linha de Tratamento",
    cast(o.value_numeric as char)as "Valor do Resultado da última Carga Viral",
-   cast(o.date_created as date) as "Data do Resultado da Carga Viral" 
+   cast(o.date_created as date) as "Data do Resultado da Carga Viral"
 from
    person p 
    inner join
@@ -84,16 +84,20 @@ from
       )
       as Viralload
       on Viralload.obs_id = o.obs_id
-   Inner join orders 
-      on orders.patient_id=p.person_id
-   Inner join drug_order
-      on drug_order.order_id=orders.order_id
-   Inner join drug_order_relationship dor
-      on drug_order.order_id=dor.drug_order_id
+   inner join 
+         orders ord
+         on ord.patient_id = pt.patient_id
+   left join orders ord2
+         on ord.patient_id=ord2.patient_id and ord.order_id<ord2.order_id
+   inner join drug_order
+         on drug_order.order_id=ord.order_id
+   inner join drug_order_relationship dor
+         on drug_order.order_id=dor.drug_order_id
       and cast(dor.date_created as date) <= '#endDate#'
    Inner join concept_view  treatment_line
       on treatment_line.concept_id = dor.treatment_line_id
    LEFT OUTER JOIN
       person_address paddress 
       ON p.person_id = paddress.person_id 
-      AND paddress.voided is false;
+      AND paddress.voided is false
+      where ord2.order_id IS NULL;
