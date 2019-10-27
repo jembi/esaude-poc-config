cd4a66a5206e3027188d6c8cff615caa0628e285
Edgêncio da Calista <edgencio@gmail.com>
2019-10-27 18:05:12 +0200

    Adds till patient state field

--- openmrs/apps/reports/sql/List_of_Patients_Summary.sql
+++ openmrs/apps/reports/sql/List_of_Patients_Summary.sql
@@ -1,6 +1,8 @@
 select  distinct enc.identifier as NID, enc.full_name as Nome, enc.date_created as "Cons. Actual",appointment.next_consultation as "Próx. Cons", enc.age as "Idade",diastolic.value_numeric as "T. Art", pregnant as "Gravidez",breast_feeding_value as "Lact",condom_value, weight.value_numeric as "Peso",
 height.value_numeric as "Alt",bperimeter.value_numeric as "PB",bmi.value_numeric as "IMC", nutritional_eval as "Aval. Nut",odema.odemas_value as "Edema",nutritional_education.received as "Ed. Nut", nutritional_supplement.supplement as "Supl", suppl_quant.value_numeric as "Supl. Quant",has_symptoms.symptoms_value as "Tem Sintomas",
-symptoms_list.symptoms as "Sintomas",date_of_diagnosis.value_datetime as "Data Diag. TB",date_of_TB_start.value_datetime,date_of_TB_start.value_datetime,state_of_TB.state,type_of_prophilaxis.type_of_pfx,state_of_prophylaxis.state,sec_efects.has_sec_efects as SEF_INH,sec_efects_ctz.has_sec_efects_ctz as SEF_CTZ, enc.encounter_id
+symptoms_list.symptoms as "Sintomas",date_of_diagnosis.value_datetime as "Data Diag. TB",date_of_TB_start.value_datetime,date_of_TB_start.value_datetime,state_of_TB.state,type_of_prophilaxis.type_of_pfx,state_of_prophylaxis.state,sec_efects.has_sec_efects as SEF_INH,sec_efects_ctz.has_sec_efects_ctz as SEF_CTZ,symptoms_of_its.has_its_symptoms as its_symptoms,
+male_syndromic_appr.approach as synd_appr_male,female_syndromic_appr.approach as synd_appr_female,op_infections.infections as infects,viral_load.value_numeric as cv,cd4.value_numeric as cd4,ast.value_numeric as ast,alt.value_numeric as alt,glicemia.value_numeric as glicemia, amilase.value_numeric as amilase,creatinina.value_numeric as creatinina,
+lab_test_other.value_numeric as outro_test, line_dispense.LD,drugs_regime.drugs,frqncy.freq, Concat('Activo em ',patient_state.patient_status) as p_state, enc.encounter_id
 from (select identifier,person.person_id,full_name,date_created,TIMESTAMPDIFF(YEAR, person.birthdate, CURDATE()) as age, encounter_id,patient_id from encounter
 inner join
 (select identifier,concat(pn.given_name," ", COALESCE(pn.middle_name,'')," ", COALESCE(pn.family_name,'')) as full_name, pn.person_id, p.birthdate from person_name pn join patient_identifier pi on pn.person_id = pi.patient_id join person p on p.person_id = pn.person_id) person
@@ -265,3 +267,102 @@ end
 as has_sec_efects_ctz
  from (select value_text,value_numeric,value_coded, concept_name_type, name, locale, encounter_id, obs_id from obs  join concept_name c on c.concept_id = obs.concept_id where concept_name_type = "FULLY_SPECIFIED" and locale = "en" and name = "SP_Side_Effects_CTZ") secondary_efects_ctz
 join (select name, concept_id from concept_name where concept_name_type = "FULLY_SPECIFIED" and locale = "en") cn_secondary_efects_ctz on cn_secondary_efects_ctz.concept_id = secondary_efects_ctz.value_coded) sec_efects_ctz on sec_efects_ctz.encounter_id = obs.encounter_id
+LEFT JOIN
+(select its_symptoms.obs_id,its_symptoms.encounter_id,
+case
+when
+cn_its_symptoms.name = "True"
+then 'SIM'
+when
+cn_its_symptoms.name = "False"
+then 'NÃO'
+end
+as has_its_symptoms
+ from (select value_text,value_numeric,value_coded, concept_name_type, name, locale, encounter_id, obs_id from obs  join concept_name c on c.concept_id = obs.concept_id where concept_name_type = "FULLY_SPECIFIED" and locale = "en" and name = "Has STI Symptoms") its_symptoms
+join (select name, concept_id from concept_name where concept_name_type = "FULLY_SPECIFIED" and locale = "en") cn_its_symptoms on cn_its_symptoms.concept_id = its_symptoms.value_coded) symptoms_of_its on symptoms_of_its.encounter_id = obs.encounter_id
+
+LEFT JOIN
+(select syndromic_appr_male.obs_id,syndromic_appr_male.encounter_id,
+case
+when
+cn_syndromic_appr_male.name = "CUM_Urethral discharge"
+then 'CUM-Corrimento uretral'
+when
+cn_syndromic_appr_male.name = "UG-Úlcera Genital ulcer"
+then 'UG-Úlcera genital' when
+cn_syndromic_appr_male.name = "ESI-Swollen Scrotum"
+then 'ESI-Escroto Inchado'
+when
+cn_syndromic_appr_male.name = "BI-Inguinal Bubo"
+then 'BI-Inguinal Bubo'
+end
+as approach
+ from (select value_text,value_numeric,value_coded, concept_name_type, name, locale, encounter_id, obs_id from obs  join concept_name c on c.concept_id = obs.concept_id where concept_name_type = "FULLY_SPECIFIED" and locale = "en" and name = "Syndromic Approach_STI_M") syndromic_appr_male
+join (select name, concept_id from concept_name where concept_name_type = "FULLY_SPECIFIED" and locale = "en") cn_syndromic_appr_male on cn_syndromic_appr_male.concept_id = syndromic_appr_male.value_coded) male_syndromic_appr on male_syndromic_appr.encounter_id = obs.encounter_id
+
+LEFT JOIN
+(select syndromic_appr_female.obs_id,syndromic_appr_female.encounter_id,
+case
+when
+cn_syndromic_appr_female.name = "CUM_Urethral discharge"
+then 'CUM-Corrimento uretral'
+when
+cn_syndromic_appr_female.name = "UG-Úlcera Genital ulcer"
+then 'UG-Úlcera genital' when
+cn_syndromic_appr_female.name = "ESI-Swollen Scrotum"
+then 'ESI-Escroto Inchado'
+when
+cn_syndromic_appr_female.name = "BI-Inguinal Bubo"
+then 'BI-Inguinal Bubo'
+end
+as approach
+ from (select value_text,value_numeric,value_coded, concept_name_type, name, locale, encounter_id, obs_id from obs  join concept_name c on c.concept_id = obs.concept_id where concept_name_type = "FULLY_SPECIFIED" and locale = "en" and name = "Syndromic Approach_STI_F") syndromic_appr_female
+join (select name, concept_id from concept_name where concept_name_type = "FULLY_SPECIFIED" and locale = "en") cn_syndromic_appr_female on cn_syndromic_appr_female.concept_id = syndromic_appr_female.value_coded) female_syndromic_appr on female_syndromic_appr.encounter_id = obs.encounter_id
+LEFT JOIN
+(select encounter_id, GROUP_CONCAT(name) as infections from (select secondary_efects.obs_id,secondary_efects.encounter_id,cn_secondary_efects.name
+ from (select value_text,value_numeric,value_coded, concept_name_type, name, locale, encounter_id, obs_id from obs  join concept_name c on c.concept_id = obs.concept_id where concept_name_type = "FULLY_SPECIFIED" and locale = "en" and name = "Coded Diagnosis") secondary_efects
+join (select name, concept_id from concept_name where concept_name_type = "FULLY_SPECIFIED" and locale = "en") cn_secondary_efects on cn_secondary_efects.concept_id = secondary_efects.value_coded) opp_infections group by encounter_id) op_infections on op_infections.encounter_id = obs.encounter_id
+LEFT JOIN
+ (select value_numeric,concept_name_type, name, locale, encounter_id, obs_id from obs  join concept_name c on c.concept_id = obs.concept_id where concept_name_type = "FULLY_SPECIFIED" and locale = "en" and name = "LO_ViralLoad" and obs.value_numeric <> 0) as viral_load on viral_load.encounter_id = obs.encounter_id
+LEFT JOIN
+ (select value_numeric,concept_name_type, name, locale, encounter_id, obs_id from obs  join concept_name c on c.concept_id = obs.concept_id where concept_name_type = "FULLY_SPECIFIED" and locale = "en" and name = "LO_CD4" and obs.value_numeric <> 0) as cd4 on cd4.encounter_id = obs.encounter_id
+ LEFT JOIN
+ (select value_numeric,concept_name_type, name, locale, encounter_id, obs_id from obs  join concept_name c on c.concept_id = obs.concept_id where concept_name_type = "FULLY_SPECIFIED" and locale = "en" and name = "LO_HB" and obs.value_numeric <> 0) as hb on hb.encounter_id = obs.encounter_id
+LEFT JOIN
+ (select value_numeric,concept_name_type, name, locale, encounter_id, obs_id from obs  join concept_name c on c.concept_id = obs.concept_id where concept_name_type = "FULLY_SPECIFIED" and locale = "en" and name = "LO_AST" and obs.value_numeric <> 0) as ast on ast.encounter_id = obs.encounter_id
+ LEFT JOIN
+ (select value_numeric,concept_name_type, name, locale, encounter_id, obs_id from obs  join concept_name c on c.concept_id = obs.concept_id where concept_name_type = "FULLY_SPECIFIED" and locale = "en" and name = "LO_ALT" and obs.value_numeric <> 0) as alt on alt.encounter_id = obs.encounter_id
+ LEFT JOIN
+ (select value_numeric,concept_name_type, name, locale, encounter_id, obs_id from obs  join concept_name c on c.concept_id = obs.concept_id where concept_name_type = "FULLY_SPECIFIED" and locale = "en" and name = "LO_GLYCEMIA(3.05-6.05mmol/L)" and obs.value_numeric <> 0) as glicemia on glicemia.encounter_id = obs.encounter_id
+ LEFT JOIN
+ (select value_numeric,concept_name_type, name, locale, encounter_id, obs_id from obs  join concept_name c on c.concept_id = obs.concept_id where concept_name_type = "FULLY_SPECIFIED" and locale = "en" and name = "LO_AMILASE(600-1600/UL)" and obs.value_numeric <> 0) as amilase on amilase.encounter_id = obs.encounter_id
+ LEFT JOIN
+ (select value_numeric,concept_name_type, name, locale, encounter_id, obs_id from obs  join concept_name c on c.concept_id = obs.concept_id where concept_name_type = "FULLY_SPECIFIED" and locale = "en" and name = "LO_CREATININE(4.2-132Hmol/L)" and obs.value_numeric <> 0) as creatinina on creatinina.encounter_id = obs.encounter_id
+ LEFT JOIN
+ (select value_numeric,concept_name_type, name, locale, encounter_id, obs_id from obs  join concept_name c on c.concept_id = obs.concept_id where concept_name_type = "FULLY_SPECIFIED" and locale = "en" and name = "LO_Other:" and obs.value_numeric <> 0) as lab_test_other on lab_test_other.encounter_id = obs.encounter_id
+
+ LEFT JOIN
+ (select  patient_id, encounter_id, GROUP_CONCAT(LD) as LD from (select drug,patient_id, encounter_id, order_id, GROUP_CONCAT(LD) as LD from (select concat(d.line_of_treatment,'-',d.dosing_instructions) as LD,d.order_id, d.encounter_id, d.patient_id, d.drug from (select drug.order_id, drug.concept_id, drug.encounter_id,drug.patient_id, drug.dose_units,
+case
+when
+drug.dosing_instructions = '{"instructions":"Month"}'
+then 'Mensal'
+when
+drug.dosing_instructions = '{"instructions":"Trimester"}'
+then 'Trimestral'
+when
+drug.dosing_instructions = '{"instructions":"Semestral"}'
+then 'Semestral'
+end
+as dosing_instructions,
+drug.quantity, drug.drug, concept_name.name as line_of_treatment from (select ord.order_id, ord.concept_id,ord.encounter_id,ord.patient_id, dose_units,dosing_instructions,quantity,dor.category_id, dor.treatment_line_id, cn.name as drug from orders ord join drug_order dorder
+on ord.order_id = dorder.order_id inner join drug_order_relationship dor on dor.drug_order_id = ord.order_id join concept_name cn on cn.concept_id = ord.concept_id and cn.locale = 'en' and cn.concept_name_type = "FULLY_SPECIFIED") drug join concept_name on concept_name.concept_id = drug.treatment_line_id and concept_name.locale = 'pt' and concept_name.concept_name_type = "FULLY_SPECIFIED") d) p_table group by order_id)p1_table group by encounter_id) line_dispense on line_dispense.encounter_id = obs.encounter_id
+LEFT JOIN
+(select order_id, encounter_id,dose_units, GROUP_CONCAT(drugs) as drugs from (select order_id, encounter_id,dose_units, GROUP_CONCAT(drug) as drugs from (select drug.order_id, drug.concept_id, drug.encounter_id,drug.patient_id, drug.dose_units,
+drug.quantity, drug.drug, concept_name.name as line_of_treatment from (select ord.order_id, ord.concept_id,ord.encounter_id,ord.patient_id, dose_units,dosing_instructions,quantity,dor.category_id, dor.treatment_line_id, cn.name as drug from orders ord join drug_order dorder
+on ord.order_id = dorder.order_id inner join drug_order_relationship dor on dor.drug_order_id = ord.order_id join concept_name cn on cn.concept_id = ord.concept_id and cn.locale = 'en' and cn.concept_name_type = "FULLY_SPECIFIED") drug join concept_name on concept_name.concept_id = drug.treatment_line_id and concept_name.locale = 'pt' and concept_name.concept_name_type = "FULLY_SPECIFIED") drug_table group by order_id) drugs_table group by drugs_table.encounter_id) drugs_regime on drugs_regime.encounter_id = obs.encounter_id
+LEFT JOIN
+(select order_id, encounter_id, patient_id, GROUP_CONCAT(freq) as freq from  (select order_id, encounter_id, patient_id, GROUP_CONCAT(freq) as freq from (select ord.order_id,ord.encounter_id,ord.patient_id,frequency,dose_units,dosing_instructions,quantity,dor.category_id, dor.treatment_line_id, cn.name as freq from orders ord join drug_order dorder
+on ord.order_id = dorder.order_id inner join drug_order_relationship dor on dor.drug_order_id = ord.order_id join order_frequency of on of.order_frequency_id = dorder.frequency join concept_name cn on cn.concept_id = of.concept_id and cn.locale = 'pt' and cn.concept_name_type = "FULLY_SPECIFIED") freq group by order_id) frequency group by encounter_id) frqncy on frqncy.encounter_id = obs.encounter_id
+LEFT JOIN
+(select * from patient_status_state where id in (select Max(id) from patient_status_state posts Group by patient_id)) patient_state on patient_state.patient_id = enc.patient_id;
