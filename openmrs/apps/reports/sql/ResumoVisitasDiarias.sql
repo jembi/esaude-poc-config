177937c68430e13dc34393541c7810880ea753dd
amosh101 <amosh101@outlook.com>
2019-11-04 10:07:55 +0200

    WIP

--- openmrs/apps/reports/sql/ResumoVisitasDiarias.sql
+++ openmrs/apps/reports/sql/ResumoVisitasDiarias.sql
@@ -1,8 +1,8 @@
-select a.identifier as NID, 
+select (@rownum:=@rownum+1) as No,a.identifier as NID, 
 b.given_name as Nome, 
 b.family_name as Apelido,
 date(c.encounter_datetime) as 'Data de Visita'
- from patient_identifier a, person_name b, encounter c 
+ from (select @rownum:=0) as init,patient_identifier a, person_name b, encounter c 
  where a.patient_id = b.person_id and b.person_id = c.patient_id and a.identifier_type = 3 
  and date(c.encounter_datetime) between '#startDate#' and '#endDate#'
  order by c.encounter_datetime desc;
