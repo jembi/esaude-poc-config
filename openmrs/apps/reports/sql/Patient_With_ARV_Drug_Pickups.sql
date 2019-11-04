afb087ed2272aa724fe2a640be02ad80044b1a52
Simao Mazive <simao.mazive@jembi.org>
2019-11-04 14:45:04 +0200

    Date issue fixed

--- openmrs/apps/reports/sql/Patient_With_ARV_Drug_Pickups.sql
+++ openmrs/apps/reports/sql/Patient_With_ARV_Drug_Pickups.sql
@@ -45,4 +45,6 @@ FROM
     erpdrug_order edo
     ON edo.patient_id = ords.patient_id
     AND edo.dispensed_date IS NOT NULL
+    AND CAST(edo.dispensed_date AS DATE) BETWEEN '#startDate#' AND '#endDate#'
+
 ;
